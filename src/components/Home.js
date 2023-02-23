28e32f282240998f2cc1138966d8e869b43f642b
daniel matama <danmatama2020@gmail.com>
2023-02-23 16:59:30 +0300

    fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     // <div className="home-content">
     <div className={styles.homecontent}>
-      <h2>Welcome to my page</h2>
+      <h2>Welcome to our page</h2>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis suscipit necessitatibus culpa rem, temporibus sed cumque eum aliquam asperiores ipsum ipsa voluptatum iure qui id, totam at officia iste assumenda vitae ad hic sunt exercitationem. Dolorum unde obcaecati hic quae ex temporibus atque itaque voluptates eveniet iusto amet ipsa exercitationem cumque reiciendis quod ut fugit repudiandae assumenda similique, minima voluptate asperiores! Fuga minima laudantium odit maiores corporis molestias doloribus tenetur necessitatibus, doloremque saepe magnam cupiditate praesentium rem, placeat unde magni! Inventore itaque autem unde in praesentium placeat incidunt eos iusto, perspiciatis quidem vitae cupiditate cumque aut quibusdam similique soluta nihil.
       </p>
