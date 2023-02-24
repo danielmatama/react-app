54bd060d316c03863a73c3b9886933b349869625
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-02-24 14:30:57 +0300

    Solve linters error

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 import React from 'react';
 
 function Home() {
@@ -6,10 +5,32 @@ function Home() {
     <div className="home-content">
       <h2>Welcome to our page</h2>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis suscipit necessitatibus culpa rem, temporibus sed cumque eum aliquam asperiores ipsum ipsa voluptatum iure qui id, totam at officia iste assumenda vitae ad hic sunt exercitationem. Dolorum unde obcaecati hic quae ex temporibus atque itaque voluptates eveniet iusto amet ipsa exercitationem cumque reiciendis quod ut fugit repudiandae assumenda similique, minima voluptate asperiores! Fuga minima laudantium odit maiores corporis molestias doloribus tenetur necessitatibus, doloremque saepe magnam cupiditate praesentium rem, placeat unde magni! Inventore itaque autem unde in praesentium placeat incidunt eos iusto, perspiciatis quidem vitae cupiditate cumque aut quibusdam similique soluta nihil.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis suscipit
+        necessitatibus culpa rem, temporibus sed cumque eum aliquam asperiores
+        ipsum ipsa voluptatum iure qui id, totam at officia iste assumenda vitae
+        ad hic sunt exercitationem. Dolorum unde obcaecati hic quae ex
+        temporibus atque itaque voluptates eveniet iusto amet ipsa
+        exercitationem cumque reiciendis quod ut fugit repudiandae assumenda
+        similique, minima voluptate asperiores! Fuga minima laudantium odit
+        maiores corporis molestias doloribus tenetur necessitatibus, doloremque
+        saepe magnam cupiditate praesentium rem, placeat unde magni! Inventore
+        itaque autem unde in praesentium placeat incidunt eos iusto,
+        perspiciatis quidem vitae cupiditate cumque aut quibusdam similique
+        soluta nihil.
       </p>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis suscipit necessitatibus culpa rem, temporibus sed cumque eum aliquam asperiores ipsum ipsa voluptatum iure qui id, totam at officia iste assumenda vitae ad hic sunt exercitationem. Dolorum unde obcaecati hic quae ex temporibus atque itaque voluptates eveniet iusto amet ipsa exercitationem cumque reiciendis quod ut fugit repudiandae assumenda similique, minima voluptate asperiores! Fuga minima laudantium odit maiores corporis molestias doloribus tenetur necessitatibus, doloremque saepe magnam cupiditate praesentium rem, placeat unde magni! Inventore itaque autem unde in praesentium placeat incidunt eos iusto, perspiciatis quidem vitae cupiditate cumque aut quibusdam similique soluta nihil.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis suscipit
+        necessitatibus culpa rem, temporibus sed cumque eum aliquam asperiores
+        ipsum ipsa voluptatum iure qui id, totam at officia iste assumenda vitae
+        ad hic sunt exercitationem. Dolorum unde obcaecati hic quae ex
+        temporibus atque itaque voluptates eveniet iusto amet ipsa
+        exercitationem cumque reiciendis quod ut fugit repudiandae assumenda
+        similique, minima voluptate asperiores! Fuga minima laudantium odit
+        maiores corporis molestias doloribus tenetur necessitatibus, doloremque
+        saepe magnam cupiditate praesentium rem, placeat unde magni! Inventore
+        itaque autem unde in praesentium placeat incidunt eos iusto,
+        perspiciatis quidem vitae cupiditate cumque aut quibusdam similique
+        soluta nihil.
       </p>
     </div>
   );
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,18 +1,17 @@
-/* eslint-disable max-len */
 import React from 'react';
 
 function Quote() {
   return (
     <figure className="quote">
       <blockquote>
-        Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding.
+        Mathematics is not about numbers, equations, computations, or
+        algorithms: it is about understanding.
       </blockquote>
       <figcaption>
         &mdash; William Paul Thurston,
         {' '}
         <cite>Mental models</cite>
         {' '}
-
       </figcaption>
     </figure>
   );
